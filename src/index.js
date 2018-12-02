da4aae4642e605911354220ed9b8e59b8e6c1a98
warpfox <tylermwatts@gmail.com>
2018-12-02 13:03:39 -0600

    fucking invisible y axis

--- src/index.js
+++ src/index.js
@@ -1,32 +1,61 @@
 import "./styles.css";
-import * as d3 from 'd3';
+import * as d3 from "d3";
 
 const req = new XMLHttpRequest();
-req.open("GET",'/src/data.json',true);
+req.open("GET", "/src/data.json", true);
 req.send();
-req.onload=function(){
+req.onload = function() {
   const json = JSON.parse(req.responseText);
   const gdpAndYears = json.data.map(d => d);
-  const padding = 50;
-  const w = (gdpAndYears.length*4) + (padding * 2)
-  const xScale = d3.scaleLinear()
-                   .domain(0,d3.max(gdpAndYears[1]))
-                   .range([padding, w - padding])
-  const yScale = d3.scaleLinear();
-  const svg = d3.select('body')
-                .append("svg")
-                .attr("id","chart")
-                .attr('width',w)
-                .attr('height', 950);
-  svg.selectAll('rect')
-     .data(gdpAndYears)
-     .enter()
-     .append('rect')
-     .attr('x', (d,i)=>xScale(d))
-     .attr('y',(d,i)=>950-d[1])
-     .attr('width',3)
-     .attr('height',(d)=>d[1])
-     .attr("id","bar")
-     .attr("fill","blue")
-     
-}
+  const w = 825;
+  const h = 500;
+  const xScale = d3
+    .scaleLinear()
+    .domain([1947, 2017])
+    .range([50, 875]);
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, 20000])
+    .range([0, 500]);
+  const xAxis = d3
+    .axisBottom(xScale)
+    .ticks(14)
+    .tickFormat(d3.format(""));
+  const yAxis = d3.axisLeft(yScale).ticks(10);
+  const svg = d3
+    .select("#bar-area")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+
+  svg
+    .selectAll("rect")
+    .data(gdpAndYears)
+    .enter()
+    .append("rect")
+    .attr("x", (d, i) => i*3)
+    .attr("y", d => h - d[1] * 0.0275)
+    .style("height", d => d[1])
+    .style("width", 2)
+    .attr("fill", "blue")
+    .attr("class", "bar")
+    .attr("data-date", d => d[0])
+    .attr("data-gdp", d => d[1]);
+
+  const chartsvg = d3
+    .select("body")
+    .append("svg")
+    .attr("height", 600)
+    .attr("width", 925);
+
+  chartsvg
+    .append("g")
+    .attr("transform", "translate(0,0)")
+    .call(xAxis)
+    .attr("id", "x-axis");
+  chartsvg
+    .append("g")
+    .attr("transform", "translate(50,0)")
+    .call(yAxis)
+    .attr("id", "y-axis");
+};
