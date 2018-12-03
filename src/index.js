bfcca64628110139db94f309248723d135e06896
warpfox <tylermwatts@gmail.com>
2018-12-02 20:57:55 -0600

    shit finally works

--- src/index.js
+++ src/index.js
@@ -7,53 +7,73 @@ req.send();
 req.onload = function() {
   const json = JSON.parse(req.responseText);
   const gdpAndYears = json.data.map(d => d);
-  const w = 825;
-  const h = 500;
+  const w = 1100;
+  const h = 600;
+  const padding = 50;
   const xScale = d3
-    .scaleLinear()
-    .domain([1947, 2017])
-    .range([50, 875]);
+    .scaleTime()
+    .domain([new Date(1947,1,1), new Date(2015,7,1)])
+    .range([50, 1050]);
+  const widthScale = d3.scaleLinear().domain([0,275]).range([50,1050])
   const yScale = d3
     .scaleLinear()
-    .domain([0, 20000])
-    .range([0, 500]);
+    .domain([0, 19000])
+    .range([550,50]);
   const xAxis = d3
     .axisBottom(xScale)
-    .ticks(14)
-    .tickFormat(d3.format(""));
-  const yAxis = d3.axisLeft(yScale).ticks(10);
+  const yAxis = d3
+    .axisLeft(yScale)
+    .ticks(10);
   const svg = d3
-    .select("#bar-area")
+    .select("#chart")
     .append("svg")
     .attr("width", w)
     .attr("height", h);
+
+  let tooltip = d3
+      .select("body")
+      .append("div")
+      .attr("class", "tooltip")
+      .attr("id", "tooltip")
+      .style("opacity", 0);
 
   svg
     .selectAll("rect")
     .data(gdpAndYears)
     .enter()
     .append("rect")
-    .attr("x", (d, i) => i*3)
-    .attr("y", d => h - d[1] * 0.0275)
-    .style("height", d => d[1])
-    .style("width", 2)
+    .attr("x", (d,i)=>widthScale(i))
+    .attr("y", d => yScale(d[1]))
+    .attr("height", d => h-padding-yScale(d[1]))
+    .attr("width", 5)
     .attr("fill", "blue")
     .attr("class", "bar")
     .attr("data-date", d => d[0])
-    .attr("data-gdp", d => d[1]);
+    .attr("data-gdp", d => d[1])
+    .on("mouseover", function(d) {
+        tooltip
+          .transition()
+          .duration(200)
+          .style("opacity", 0.9);
+        tooltip
+          .html(`$${d[1]} Billion\n${d[0]}`)
+          .style("left", d3.event.pageX + 20 + "px")
+          .style("top", d3.event.pageY + 20 + "px");
+        tooltip.attr("data-date", d[0]);
+      })
+      .on("mouseout", function(d) {
+        tooltip
+          .transition()
+          .duration(400)
+          .style("opacity", 0);
+      });
 
-  const chartsvg = d3
-    .select("body")
-    .append("svg")
-    .attr("height", 600)
-    .attr("width", 925);
-
-  chartsvg
+  svg
     .append("g")
-    .attr("transform", "translate(0,0)")
+    .attr("transform", "translate(0,550)")
     .call(xAxis)
     .attr("id", "x-axis");
-  chartsvg
+svg
     .append("g")
     .attr("transform", "translate(50,0)")
     .call(yAxis)
